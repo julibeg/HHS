e661348eecc53257ffd9bc23958194dd63b38d07
Julian Libiseller-Egger <37619875+zeawoas@users.noreply.github.com>
2021-04-08 16:50:25 +0100

    commented distance script

--- scripts/pw_bin_hamm_dists_from_gt.sh
+++ scripts/pw_bin_hamm_dists_from_gt.sh
@@ -2,10 +2,23 @@
 
 gt_file=$1
 
-if (file $gt_file | grep -q compressed )
+# figure out which cat to use based on whether the genotype 
+# matrix is gzipped.
+if file $gt_file | grep -q gzip
 then
      my_cat=zcat
 else my_cat=cat
 fi
 
- cat <($my_cat $gt_file | head -n1 | sed 's/^/,/') <(paste -d',' <($my_cat $gt_file | head -n1 | tr ',' '\n') <(binary_hamming_dist -i <($my_cat $gt_file | sed '1d' | cut -d: -f2) -T -t 6))
+# heavy use of process substitution:
+# *) we need to get the header from the gt file and add a comma to
+#    the beginning for the absent label of the index column. 
+#    --> <($my_cat $gt_file | head -n1 | sed 's/^/,/')
+# *) `binary_hamming_dist` expects bare binary strings of '0's and '1's
+#    --> we need to remove the header and the variant IDs from the gt matrix
+#    --> <($my_cat $gt_file | sed '1d' | cut -d: -f2)
+# *) add the samples as index column to the calculated distances with `paste`
+#    --> <(paste -d',' <($my_cat $gt_file | head -n1 | tr ',' '\n') <(...)
+ cat <($my_cat $gt_file | head -n1 | sed 's/^/,/') \
+     <(paste -d',' <($my_cat $gt_file | head -n1 | tr ',' '\n') \
+     <(binary_hamming_dist -i <($my_cat $gt_file | sed '1d' | cut -d: -f2) -T -t 6))
